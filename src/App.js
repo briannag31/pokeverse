88454b85fb247e81913fdbf6266b730927eff66b
Brianna Gaines <briannagaines31@gmail.com>
2022-12-22 12:33:22 -0800

    finishes steps 1-4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
-const LIMIT = 150;
+const LIMIT = 5;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
-
+  const [pokemon, setPokemon] = useState([])
+  // const [filteredPoke, setFilteredPoke] = useState([])
+  async function fetchPokemon(){
+    try{
+      const res = await fetch(pokeApi)
+      const data = await res.json()
+      setPokemon(data.results)
+      // setFilteredPoke(data.results)
+      // console.log(data.results)
+      // console.log("pokemon is",pokemon)
+    } catch(err){
+      console.log(err)
+    }
+  }
+  useEffect(() =>{
+    fetchPokemon()
+  }, [])
   return (
     <div data-testid="app">
       <Navigation />
 
       <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      {pokemon.map((singlePokemon, idx)=>{
+          return(
+            <PokemonCard  key={idx} url={singlePokemon.url} name={singlePokemon.name} setPokemon={setPokemon} pokemon={pokemon}/>
+          )
+        })}
+      
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Card} from "react-bootstrap"
 
-function PokemonCard({ url, name }) {
-
+function PokemonCard({url, name, setPokemon}) {
+  const [poke, setPoke] = useState(null)
+  async function fetchOnePoke(){
+    try{
+      const res = await fetch(url)
+      const data = await res.json()
+      setPoke(data)
+  
+      console.log("data is: ",data)
+      console.log("poke is: ",poke)
+      // console.log("pokemon is",pokemon)
+    } catch(err){
+      console.log(err)
+    }
+  }
+  useEffect(() =>{
+    fetchOnePoke()
+  }, [])
   return (
-    <div>
-        pokemon card
-    </div>
+   <Card>
+    <Card.Img src={poke?.sprites.front_default} alt={name} onClick={()=> console.log(poke.abilities[0].ability.name)}/>
+    {/* <Card.Title>{poke?.abilities[0].ability.name}</Card.Title> */}
+    
+    <Card.Text>
+      <ul>
+      {poke?.abilities.map((abil, i) => (
+        <li key={i}>{abil.ability.name}</li>
+      ))}
+      </ul>
+    </Card.Text>
+   </Card>
   );
 }
 
